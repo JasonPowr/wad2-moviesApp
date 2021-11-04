2bdb89add7d236937f95cfe98615ef287cb375c3
Jason Power <20076537@mail.wit.ie>
2021-11-04 16:28:11 +0000

    Added Upcoming moview page.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Redirect, Switch, Link } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
+import UpcomingMoviesPage from "./pages/upcomingMoviesPage";
 import FavoriteMoviesPage from "./pages/favoriteMoviesPage"; // NEW
 import MovieReviewPage from "./pages/movieReviewPage";
 import SiteHeader from './components/siteHeader'
@@ -13,6 +14,7 @@ const App = () => {
           <SiteHeader />      {/* New Header  */}
           <Switch>
       <Route path="/reviews/:id" component={MovieReviewPage} />
+      <Route exact path="/movies/upcoming" component={UpcomingMoviesPage} />
         <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
         <Route path="/movies/:id" component={MoviePage} />
         <Route exact path="/" component={HomePage} />
